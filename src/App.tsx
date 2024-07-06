c96997eb3157c38be79141cf964d793e7fe8af90
Bug-Finderr <sudu.boi.2k5@gmail.com>
2024-07-06 18:00:02 +0530

    Working (first) API call to OpenAI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import './App.css';
 
+const getMessages = async () => {
+    const options = {
+        method: 'POST',
+        body: JSON.stringify({
+            message: 'Hello, sir!'      // Will have a message from the input
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    try {
+        const response = await fetch('http://localhost:8000/chat', options)
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 const App: React.FC = () => {
     return (
         <div className="app">
@@ -20,7 +39,7 @@ const App: React.FC = () => {
                 <div className="bottom-section">
                     <div className="input-container">
                         <input />
-                        <div id="submit">⪢</div>
+                        <div id="submit" onClick={getMessages}>⪢</div>
                     </div>
                     <p className="info">Be sure to set up the API key before usage</p>
                 </div>
--- src/server.js
+++ src/server.js
@@ -1,11 +1,39 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
+dotenv.config()
 
 const PORT = 8000
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+app.post('/chat', async (req, res) => {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                model: 'gpt-4o',
+                messages: [{
+                        role: 'user',
+                        content: 'Hey sir'
+                    }]
+            })
+        }
+
+        try {
+            const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+            const data = await response.json()
+            res.send(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
